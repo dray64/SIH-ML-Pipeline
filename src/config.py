4fe32ad813641767e3ee2e83718dc92410f2919a
Debmalya Ray <235037096+dray64@users.noreply.github.com>
2026-09-10 21:31:19 +0530

    src/config fix

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ from pathlib import Path
 # Base Paths
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
-RAW_DATA_PATH = DATA_DIR / "raw" / "D:\\flash-flood-early-warning\\data\\flash_flood_dataset_sensor_ready.csv"
+RAW_DATA_PATH = DATA_DIR / "raw" / "D:\\SIH ML Pipeline\\data\\flash_flood_dataset_sensor_ready.csv"
 CACHE_BUFFER_PATH = DATA_DIR / "cache" / "buffer_state.json"
-MODEL_PATH = BASE_DIR / "models" / "D:\\flash-flood-early-warning\\models\\flash_flood_dual_stage_model.joblib"
+MODEL_PATH = BASE_DIR / "models" / "D:\\SIH ML Pipeline\\models\\flash_flood_dual_stage_model.joblib"
 
 # Hydrological Thresholds (in feet)
 ACTION_STAGE_FT = 10.0
